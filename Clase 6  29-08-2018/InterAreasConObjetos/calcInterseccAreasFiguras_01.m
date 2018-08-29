cecc6aafcf2dbb16e6b9525c1f713770138b61f4
sdist <sdist@itam.mx>
2018-08-29 10:28:47 -0500

    Clase 6

---
+++ Clase 6  29-08-2018/InterAreasConObjetos/calcInterseccAreasFiguras_01.m
@@ -0,0 +1,51 @@
+%%Esta es la que hace la chamba NO ES LO MÁS EFICIENTE
+%% Tenemos que hacerlo más eficiente
+
+%%Estamos construyendo la caja
+%%nf es el número de figuras
+function [areas,tamRect] = calcInterseccAreasFiguras_01(figs,N)
+  nf = size(figs,2);
+  arr_xMax = zeros(1,nf);
+  arr_xMin = zeros(1,nf);
+  arr_yMax = zeros(1,nf);
+  arr_yMin = zeros(1,nf);
+    
+  %%Este no es el ineficiente
+  %%A cada figura le pide los valores extremos
+  for k = 1:nf
+    tam = figs{k}.valoresExtremos();
+    arr_xMin(k) = tam(1,1);
+    arr_yMin(k) = tam(2,1);
+    arr_xMax(k) = tam(1,2);
+    arr_yMax(k) = tam(2,2);
+  end
+  xmin = min(arr_xMin);
+  xmax = max(arr_xMax);
+  ymin = min(arr_yMin);
+  ymax = max(arr_yMax);
+
+  dx = xmax - xmin;
+  dy = ymax - ymin;
+    
+  %%Crea una matriz de números aleatorios... Son los puntos x,y
+  p = [xmin + dx*rand(1,N);ymin + dy*rand(1,N)]; 
+
+  %%
+  c = zeros(1,nf);
+  dos_a_la_nf = 2^nf;
+  vd = cumprod([1;2*ones(nf-1,1)]);
+  d = zeros(1,dos_a_la_nf);
+  
+  %%Pensar cómo arreglar este for
+  for t = 1:N
+    for k = 1:nf
+      c(k) = figs{k}.contiene(p(:,t));
+    end
+    w = c*vd; %%Multiplica por el vd, ya están arreglados para que sea un producto punto
+              %% Es el valor donde ocurrió la intersección
+    if w == 0, w = dos_a_la_nf; end 
+    d(w) = d(w) + 1;
+  end
+  tamRect = dx * dy;
+  areas = d * tamRect / N;
+end
---
+++ Clase 6  29-08-2018/InterAreasConObjetos/tstCalcInterseccAreasFiguras_03.m
@@ -0,0 +1,37 @@
+%tstCalcInterseccAreasFiguras_02
+%
+N = 10000;
+figs = {};
+% figs(1) = {Circulo([0.0;0.0],1.0)};
+% figs(2) = {Circulo([0.0;1.0],1.0)};
+% figs(3) = {Circulo([0.5;0.5*sqrt(3)],1.0)};
+% figs(1) = {ElipseCanonica([1.0;0.0],2.0,1.0)};
+% figs(4) = {RectanguloCanonico([2.0;1.0],2.0,3.0)};
+% figs(1) = {RomboCanonico([0.0;0.0],2.0,3.0)};
+% figs(6) = {Triangulo([0.0;0.0],[1.0;0.0],[0.0;1.0])};
+Z = exp(j*2*pi*(0:4)/5); V = [real(Z);imag(Z)];
+figs(1) = {Poligono(V)};
+veces = 500;
+nf   = size(figs,2);
+area = zeros(veces,2^nf);
+tic
+k = 1;
+parfor vez = 1:veces
+    [a,tamRect] = calcInterseccAreasFiguras_01(figs,N);
+    area(vez,:) = a;
+    if mod(vez,50) == 0
+        %for k = 1:2^nf
+           fprintf('vez: %4d, area(%3d): %12.6f\n',vez,k,a(k))
+        %end     
+    end
+end    
+toc
+prom_area    = mean(area);
+desvStd_area = std(area);
+for k = 1:2^nf
+    fprintf('para intersección %3d, promedio de area: %12.6f,   desvStd: %12.6f\n', ...
+         k,prom_area(k), desvStd_area(k))
+end
+%
+% ================= Fin de tst ====================
+%
